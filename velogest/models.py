d4fbe26d1d41ddbbaf56119e73315db10d9a4960
Weibing WANG <wwang@idmog.com>
2022-10-06 14:34:56 +0200

    Add type.

--- velogest/models.py
+++ velogest/models.py
@@ -6,8 +6,16 @@ from django.db import models
 
 
 class Sensor(models.Model):
-    name = models.TextField()
-    created_at = models.DateTimeField()
-    modified_at = models.DateTimeField()
+    name = models.TextField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
     latitude = models.FloatField()
     longitude = models.FloatField()
+    type = models.CharField(
+        max_length=20,
+        choices=[
+            ("T1", "t1"),
+            ("T2", "t2"),
+        ],
+        default="T1"
+    )
