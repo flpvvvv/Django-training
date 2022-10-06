61b5e7aaa1c1d625043593df7159b2f88d3c18b2
Weibing WANG <wwang@idmog.com>
2022-10-06 14:03:31 +0200

    Lint

--- velogest/models.py
+++ velogest/models.py
@@ -3,11 +3,11 @@ from statistics import mode
 from django.db import models
 
 # Create your models here.
+
+
 class Sensor(models.Model):
     name = models.TextField()
     created_at = models.DateTimeField()
     modified_at = models.DateTimeField()
     latitude = models.FloatField()
     longitude = models.FloatField()
-
-
--- velogest/views.py
+++ velogest/views.py
@@ -2,5 +2,7 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def ma_vue(request):
     return HttpResponse("Hello")
