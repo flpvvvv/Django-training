22ba2051de15b70bc96ba4b6e855c1cbff3f058c
Weibing WANG <wwang@idmog.com>
2022-10-06 16:23:45 +0200

    Add sensor detail view by sensor name

--- velogest/urls.py
+++ velogest/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from velogest.views import velogest_home, sensor_list
+from velogest.views import velogest_home, sensor_list, SensorView
 
 app_name = 'velogest'
 urlpatterns = [
-    path('', velogest_home, name="home" ),
-    path('list', sensor_list, name='list'),
+    path('', velogest_home, name="home"),
+    path('list', sensor_list, name="list"),
+    path('sensor/<str:sensor_name>', SensorView.as_view(), name="sensor"),
 ]
--- velogest/views.py
+++ velogest/views.py
@@ -1,12 +1,15 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.views import View
 from velogest.models import Sensor
+from django.shortcuts import resolve_url
 
 # Create your views here.
 
 
 def home(request):
     return HttpResponse("Home page")
+
 
 def velogest_home(request):
     return HttpResponse("Velogest home page")
@@ -18,3 +21,17 @@ def sensor_list(request):
         "sensors": sensors
     }
     return render(request, 'sensor_list.html', context)
+
+
+class SensorView(View):
+    def get(self, request, sensor_name):
+        # print(sensor_name)
+        sensor = Sensor.objects.get(name=sensor_name)
+        context = {
+            "sensor_name": sensor.name,
+            "sensor_type": sensor.type,
+            "latitude": sensor.latitude,
+            "longitude": sensor.longitude,
+        }
+        return render(request, 'sensor_detail.html', context)
+        # return HttpResponseRedirect(resolve_url('velogest:sensor', sensor_name))
