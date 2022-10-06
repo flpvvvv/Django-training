bce1d0bb8793f1c1e0a523e7fde8b37ab61e2e50
Weibing WANG <wwang@idmog.com>
2022-10-06 12:17:17 +0200

    Add Sensor model.

--- velogest/admin.py
+++ velogest/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from velogest.models import Sensor
 
 # Register your models here.
+admin.site.register(Sensor)
--- velogest/models.py
+++ velogest/models.py
@@ -1,3 +1,13 @@
+from pyexpat import model
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+class Sensor(models.Model):
+    name = models.TextField()
+    created_at = models.DateTimeField()
+    modified_at = models.DateTimeField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+
