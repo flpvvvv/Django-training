455e9d96e360522d4f9e550f8403b8f0b89ba6c1
Weibing WANG <wwang@idmog.com>
2022-10-06 16:28:46 +0200

    Add sensor detail view by ID.

--- velogest/urls.py
+++ velogest/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from velogest.views import velogest_home, sensor_list, SensorView
+from velogest.views import velogest_home, sensor_list, SensorView, SensorViewID
 
 app_name = 'velogest'
 urlpatterns = [
     path('', velogest_home, name="home"),
     path('list', sensor_list, name="list"),
     path('sensor/<str:sensor_name>', SensorView.as_view(), name="sensor"),
+    path('sensor_id/<int:pk>', SensorViewID.as_view(), name="sensor_id"),
 ]
--- velogest/views.py
+++ velogest/views.py
@@ -23,6 +23,19 @@ def sensor_list(request):
     return render(request, 'sensor_list.html', context)
 
 
+class SensorViewID(View):
+    def get(self, request, pk):
+        # print(sensor_name)
+        sensor = Sensor.objects.get(pk=pk)
+        context = {
+            "sensor_name": sensor.name,
+            "sensor_type": sensor.type,
+            "latitude": sensor.latitude,
+            "longitude": sensor.longitude,
+        }
+        return render(request, 'sensor_detail.html', context)
+
+
 class SensorView(View):
     def get(self, request, sensor_name):
         # print(sensor_name)
